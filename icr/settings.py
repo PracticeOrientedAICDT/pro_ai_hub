5d45171ef669022318fce754a24226791657509b
DelmiroDaladier <daladiersampaio@gmail.com>
2023-02-24 13:48:00 +0000

    changes the trusted origins

--- icr/settings.py
+++ icr/settings.py
@@ -145,3 +145,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
+
+CSRF_TRUSTED_ORIGINS = ['https://cdt-icr.onrender.com']
